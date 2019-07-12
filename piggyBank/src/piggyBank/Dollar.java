6d436ef227dcb933165910ac653007d703e8d315
tthompson082 <tthompson082@gmail.com>
2019-07-12 11:17:39 -0400

    :label: Create Dollar Class
    Create Dollar class with necessary fields and methods

--- piggyBank/src/piggyBank/Dollar.java
+++ piggyBank/src/piggyBank/Dollar.java
@@ -1,2 +1,39 @@
 package piggyBank;
 
+public class Dollar
+{
+    // fields
+    private static int totalDollars = 0;
+    private int dollarQuantity;
+    public double value = 1.00;
+
+    // constructor
+    public Dollar(int dollarQuantity)
+    {
+        this.dollarQuantity = dollarQuantity;
+        totalDollars += dollarQuantity;
+    }
+
+    // methods
+    public int getDollarQuantity()
+    {
+        return dollarQuantity;
+    }
+
+    public double getQuantityValue()
+    {
+        return dollarQuantity * value;
+    }
+
+    public double getTotalValue()
+    {
+        return value * totalDollars;
+    }
+
+    @Override
+    public String toString()
+    {
+        String rtnStr = "$" + dollarQuantity;
+        return rtnStr;
+    }
+}
