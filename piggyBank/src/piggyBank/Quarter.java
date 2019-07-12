fb79d5f12b117ff4da2e97323dd3bef8813214e9
tthompson082 <tthompson082@gmail.com>
2019-07-12 11:23:33 -0400

    :label: Create Quarter Class
    Create Quarter Class with necessary fields and methods

---
+++ piggyBank/src/piggyBank/Quarter.java
@@ -0,0 +1,45 @@
+package piggyBank;
+
+public class Quarter
+{
+    // fields
+    private static int totalQuarters = 0;
+    private int quarterQuantity;
+    public double value = 0.25;
+
+    // constructor
+    public Dollar(int quarterQuantity)
+    {
+        this.quarterQuantity = quarterQuantity;
+        totalQuarters += quarterQuantity;
+    }
+
+    // methods
+    public int getQuarterQuantity()
+    {
+        return quarterQuantity;
+    }
+
+    public double getQuantityValue()
+    {
+        return quarterQuantity * value;
+    }
+
+    public double getTotalValue()
+    {
+        return value * totalQuarters;
+    }
+
+    @Override
+    public String toString()
+    {
+        if (quarterQuantity == 1)
+        {
+            String rtnStr = quarterQuantity + " Quarter";
+        } else 
+        {
+            String rtnStr = quarterQuantity + " Quarters";
+        }
+        return rtnStr;
+    }
+}
