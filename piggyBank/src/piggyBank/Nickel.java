2cb615d7489316f76664f2a3c4787207dbb60075
tthompson082 <tthompson082@gmail.com>
2019-07-12 11:30:52 -0400

    :label: Create Nickel Class
    Create Nickel Class with necessary fields and methods

---
+++ piggyBank/src/piggyBank/Nickel.java
@@ -0,0 +1,45 @@
+package piggyBank;
+
+public class Nickel
+{
+    // fields
+    private static int totalNickels = 0;
+    private int nickelQuantity;
+    public double value = 0.25;
+
+    // constructor
+    public Nickel(int nickelQuantity)
+    {
+        this.nickelQuantity = nickelQuantity;
+        totalQuarters += nickelQuantity;
+    }
+
+    // methods
+    public int getQNickelQuantity()
+    {
+        return nickelQuantity;
+    }
+
+    public double getQuantityValue()
+    {
+        return nickelQuantity * value;
+    }
+
+    public double getTotalValue()
+    {
+        return value * totalNickels;
+    }
+
+    @Override
+    public String toString()
+    {
+        if (nickelQuantity == 1)
+        {
+            String rtnStr = nickelQuantity + " Nickel";
+        } else 
+        {
+            String rtnStr = nickelQuantity + " Nickels";
+        }
+        return rtnStr;
+    }
+}
